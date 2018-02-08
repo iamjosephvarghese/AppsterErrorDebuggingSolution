67c9e6e1fef0d0cd05742f98c03e888ba187309b
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-02-08 21:55:07 +0530

    Initial commit

---
+++ app/src/test/java/techkshetra18/appster/appstererror/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package techkshetra18.appster.appstererror;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
