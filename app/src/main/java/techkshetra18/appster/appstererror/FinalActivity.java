8d6a4ae1e765aff58a2ff02148292ea249d072c5
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-02-09 05:53:40 +0530

    almost done coding without errors

--- app/src/main/java/techkshetra18/appster/appstererror/FinalActivity.java
+++ app/src/main/java/techkshetra18/appster/appstererror/FinalActivity.java
@@ -3,20 +3,29 @@ package techkshetra18.appster.appstererror;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FinalActivity extends AppCompatActivity {
 
+    TextView final2;
     Integer count;
+
+    TextView final1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
 
-        Intent dataCollect = getIntent();
+        Intent dataCollect =  getIntent();
+        count = dataCollect.getIntExtra("noSelected",0);
 
-//        count = dataCollect.getStringExtra("noSelected");
-//        TODO heeerreeee
+
+        final1 = findViewById(R.id.final1);
+        final2 = findViewById(R.id.final2);
+
+
+        final2.setText(count);
 
     }
 }
